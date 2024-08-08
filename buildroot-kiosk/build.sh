1c064c52f4787b0c03f3391ef0bc624aa9cd7539
Sander de Leeuw <s.deleeuw@gmail.com>
2024-08-08 22:14:11 +0200

    feat: buildroot-kiosk: set download folder using environment variable

--- buildroot-kiosk/build.sh
+++ buildroot-kiosk/build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 set -e -x
 
+export BR2_DL_DIR=/build/downloads
+
 # clean up previous build
 test -d "buildroot" && \
 rm -rf "buildroot"
 
 # checkout buildroot source
-git clone --depth 1 git@gitlab.com:buildroot.org/buildroot.git
-
-# re-use downloads folder
-ln -sf downloads buildroot/dl
+git clone --depth 1 https://gitlab.com/buildroot.org/buildroot.git
 
 # copy configs
 cp -rv configs buildroot/
 
 # build
+cd buildroot
 make raspberrypizero2w_defconfig
 make

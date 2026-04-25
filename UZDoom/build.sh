d322bbc572b23ba2e536dad1a4b8872a1be5ef84
Sander de Leeuw <sander@peakfijn.nl>
2026-04-25 11:58:03 +0200

    feat(UZDoom): initial scripts

---
+++ UZDoom/build.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+set -e
+
+NAME=UZDoom
+VERSION=4.14.3
+ARCH=$(uname -m)
+
+# download sources
+test -e ${NAME}-${VERSION}.tar.gz || \
+  wget "https://github.com/UZDoom/${NAME}/archive/refs/tags/${VERSION}.tar.gz" \
+    -O ${NAME}-${VERSION}.tar.gz
+
+# check integrity
+sha256sum -c checksums-sha256.txt
+
+# clean up previous build
+rm -rf ${NAME}-${VERSION}
+
+# extract tarball
+tar xzf ${NAME}-${VERSION}.tar.gz
+
+# --------
+
+ZMUSIC_VERSION=1.3.0
+ZMUSIC_DIRNAME=ZMusic-${ZMUSIC_VERSION}
+
+test -e ${ZMUSIC_DIRNAME}.tar.gz || \
+  wget "https://github.com/ZDoom/ZMusic/archive/refs/tags/${ZMUSIC_VERSION}.tar.gz" \
+    -O ${ZMUSIC_DIRNAME}.tar.gz
+
+rm -rf ${ZMUSIC_DIRNAME}
+tar xzf ${ZMUSIC_DIRNAME}.tar.gz
+
+mkdir ${ZMUSIC_DIRNAME}/build
+( cd ${ZMUSIC_DIRNAME}/build
+  cmake -DCMAKE_BUILD_TYPE=Release ..
+  cmake --build .
+  sudo make install
+)
+
+# --------
+
+# setup
+mkdir ${NAME}-${VERSION}/build
+cd ${NAME}-${VERSION}/build
+
+cmake \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+  -G Ninja \
+  ..
+
+# compile
+cmake --build .
